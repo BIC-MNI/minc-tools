BIC-MNI/minc-tools

commit 89bcfe73b705ebc84d7442cec0cff78aa7217e15

When using mincresample, the tolerance/precision (ftol) for calculating the position of resampled voxels is based on the step sizes of the deformation grid. These step sizes can differ from the input volume being resampled. This commit (together with a commit in the libminc repository) base the ftol variable on the step sizes of the input volume rather than the deformation grid.

progs/mincresample/resample_volumes.c
@@ -474,6 +474,11 @@ void get_slice(long slice_num, VVolume *in_vol, VVolume *out_vol,
    long irow, icol;
    int all_linear;
    int idim;
+   
+   VIO_Real separations[WORLD_NDIMS];
+   int  idim_in;
+   char dimname[MAX_NC_NAME];
+   int  dim[MAX_VAR_DIMS], dimid;
 
    /* Coordinate vectors for stepping through slice */
    Coord_Vector zero = {0, 0, 0};
@@ -516,6 +521,26 @@ void get_slice(long slice_num, VVolume *in_vol, VVolume *out_vol,
    *maximum = -DBL_MAX;
    *minimum =  DBL_MAX;
 
+   /* Get the steps sizes (separations) of the input volume in order
+      to get an appropriate error margin (ftol) for the function
+      grid_inverse_transform_point */
+   for (idim_in=0; idim_in < in_vol->file->ndims; idim_in++) {
+           
+      /* Get size of dimension */
+      (void) ncdiminq(in_vol->file->mincid, dim[idim_in], dimname, 
+                      &in_vol->file->nelements[idim]);
+
+      /* Check for existence of variable */
+      dimid = ncvarid(in_vol->file->mincid, dimname);
+      if (dimid == MI_ERROR) continue;
+
+      /* Get attributes from variget_file_infoable */
+      (void) miattget1(in_vol->file->mincid, dimid, MIstep, 
+                       NC_DOUBLE, &separations[in_vol->file->world_axes[idim_in]]);
+      if (separations[in_vol->file->world_axes[idim_in]] == 0.0)
+         separations[in_vol->file->world_axes[idim_in]] = 1.0;
+   }
+
    /* Loop over rows of slice */
 
    for (irow=0; irow < slice->size[SLICE_ROW]; irow++) {
@@ -534,7 +559,7 @@ void get_slice(long slice_num, VVolume *in_vol, VVolume *out_vol,
          for (idim=0; idim<WORLD_NDIMS; idim++) 
             transf_coord[idim]=coord[idim];
          if (!all_linear) {
-            DO_TRANSFORM(transf_coord, &total_transf, transf_coord);
+            DO_TRANSFORM_WITH_INPUT_STEPS(transf_coord, &total_transf, transf_coord, separations);
          }
 
          /* Do interpolation */

progs/mincresample/mincresample.c
@@ -1142,8 +1142,8 @@ static void transform_volume_def(Transform_Info *transform_info,
                input_volume_def->dircos[jdim][idim];
 
       /* VIO_Transform origin vector */
-      DO_INVERSE_TRANSFORM(transformed_origin, 
-                           transform_info->transformation, origin);
+      DO_INVERSE_TRANSFORM_WITH_INPUT_STEPS(transformed_origin,
+                           transform_info->transformation, origin, input_volume_def->step);
 
       /* Loop over dimensions */
       for (idim=0; idim < WORLD_NDIMS; idim++) {
@@ -1156,7 +1156,7 @@ static void transform_volume_def(Transform_Info *transform_info,
          VECTOR_SCALAR_MULT(vector, input_volume_def->dircos[idim],
                             nelements * input_volume_def->step[idim]);
          VECTOR_ADD(vector, vector, origin);
-         DO_INVERSE_TRANSFORM(vector, transform_info->transformation, vector);
+         DO_INVERSE_TRANSFORM_WITH_INPUT_STEPS(vector, transform_info->transformation, vector, input_volume_def->step);
          VECTOR_DIFF(vector, vector, transformed_origin);
 
          /* Calculate length of transformed axis - new step value */

progs/mincresample/mincresample.h
@@ -272,9 +272,19 @@ typedef struct {
       coord[XCOORD], coord[YCOORD], coord[ZCOORD], \
       &result[XCOORD], &result[YCOORD], &result[ZCOORD])
 
+#define DO_TRANSFORM_WITH_INPUT_STEPS(result, transformation, coord, input_volume_steps) \
+   general_transform_point_with_input_steps(transformation, \
+      coord[XCOORD], coord[YCOORD], coord[ZCOORD], input_volume_steps, \
+      &result[XCOORD], &result[YCOORD], &result[ZCOORD])
+
 #define DO_INVERSE_TRANSFORM(result, transformation, coord) \
    general_inverse_transform_point(transformation, \
       coord[XCOORD], coord[YCOORD], coord[ZCOORD], \
+      &result[XCOORD], &result[YCOORD], &result[ZCOORD])
+
+#define DO_INVERSE_TRANSFORM_WITH_INPUT_STEPS(result, transformation, coord, input_volume_steps) \
+   general_inverse_transform_point_with_input_steps(transformation, \
+      coord[XCOORD], coord[YCOORD], coord[ZCOORD], input_volume_steps, \
       &result[XCOORD], &result[YCOORD], &result[ZCOORD])
 
 #define IS_LINEAR(transformation) \

commit 84f9503ada5ff64e8c5c0126d8a4b3f460a162af

Fixed bug introduced in 89bcfe73b705ebc84d7442cec0cff78aa7217e15

progs/mincresample/resample_volumes.c
@@ -479,7 +479,8 @@ void get_slice(long slice_num, VVolume *in_vol, VVolume *out_vol,
    int  idim_in;
    char dimname[MAX_NC_NAME];
    int  dim[MAX_VAR_DIMS], dimid;
-
+   int  imgid;
+   int  ndims;
    /* Coordinate vectors for stepping through slice */
    Coord_Vector zero = {0, 0, 0};
    Coord_Vector column = {0, 0, 1};
@@ -520,6 +521,9 @@ void get_slice(long slice_num, VVolume *in_vol, VVolume *out_vol,
    /* Initialize maximum and minimum */
    *maximum = -DBL_MAX;
    *minimum =  DBL_MAX;
+   
+   imgid = ncvarid(in_vol->file->mincid, MIimage);
+   ncvarinq(in_vol->file->mincid, imgid, NULL, NULL, &ndims, dim, NULL);
 
    /* Get the steps sizes (separations) of the input volume in order
       to get an appropriate error margin (ftol) for the function
@@ -537,8 +541,9 @@ void get_slice(long slice_num, VVolume *in_vol, VVolume *out_vol,
       /* Get attributes from variget_file_infoable */
       (void) miattget1(in_vol->file->mincid, dimid, MIstep, 
                        NC_DOUBLE, &separations[in_vol->file->world_axes[idim_in]]);
+
       if (separations[in_vol->file->world_axes[idim_in]] == 0.0)
-         separations[in_vol->file->world_axes[idim_in]] = 1.0;
+          separations[in_vol->file->world_axes[idim_in]] = 1.0;
    }
 
    /* Loop over rows of slice */