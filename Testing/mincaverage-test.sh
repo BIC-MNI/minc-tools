19f44a4a649c4c47b7213830183bb2512601422d
Robert D. Vincent <robert.d.vincent@mcgill.ca>
2016-03-14 10:57:46 -0400

    Improve mincaverage test to check functionality of -avgdim option.

--- Testing/mincaverage-test.sh
+++ Testing/mincaverage-test.sh
@@ -6,6 +6,11 @@ fi;
 if [[ ! -x $MINCSTATS_BIN ]]; then
     MINCSTATS_BIN=`which mincstats`;
 fi;
+
+if [[ ! -x $MINCINFO_BIN ]]; then
+    MINCINFO_BIN=`which mincinfo`;
+fi
+
 # Test the standard (no-normalize) case. This has always worked.
 $MINCAVERAGE_BIN -clobber mincaverage-in0.mnc mincaverage-in1.mnc mincaverage-out.mnc
 r1=`$MINCSTATS_BIN -quiet -sum mincaverage-out.mnc`
@@ -19,6 +24,43 @@ r2=`$MINCSTATS_BIN -quiet -sum mincaverage-out.mnc`
 if [[ $r2 != "-22.25" ]]; then
   echo "Problem with normalized average:" $r2
   exit 1;
+fi;
+
+# Now test new avgdim case. This would fail until 3 March 2016.
+$MINCAVERAGE_BIN -avgdim time -clobber test-4d.mnc mincaverage-out.mnc
+if [$? -ne 0 ]; then
+  echo "Problem running mincaverage."
+  exit 1;
+fi;
+r3=`$MINCSTATS_BIN -quiet -max mincaverage-out.mnc`
+if [[ $r3 != "2" ]]; then
+  echo "Problem with maximum value:" $r3
+  exit 1;
+fi;
+r4=`$MINCSTATS_BIN -quiet -min mincaverage-out.mnc`
+if [[ $r4 != "2" ]]; then
+  echo "Problem with minimum value:" $r4
+  exit 1;
+fi;
+r5=`$MINCINFO_BIN -dimlength time test-4d.mnc`
+if [[ $r5 != "5" ]]; then
+   echo "Problem with time:", $r5
+   exit 1;
+fi;
+r6=`$MINCINFO_BIN -dimlength time mincaverage-out.mnc`
+if [ $? -ne 1 ]; then
+   echo "Problem with time:", $r6
+   exit 1;
+fi;
+r7=`$MINCINFO_BIN -dimlength time-width mincaverage-out.mnc`
+if [ $? -ne 1 ]; then
+   echo "Problem with time-width:", $r7
+   exit 1;
+fi;
+r8=`$MINCINFO_BIN -varvalues time-width mincaverage-out.mnc`
+if [ $? -ne 1 ]; then
+   echo "Problem with time-width:", $r8
+   exit 1;
 fi;
 echo "OK."
 exit 0
