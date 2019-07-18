74538369258ab03d3c5539042efacc2ca7f07fce
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2019-07-18 13:35:15 -0400

    Fixed minclookup test

--- Testing/minclookup-labels-test.sh
+++ Testing/minclookup-labels-test.sh
@@ -3,7 +3,9 @@
 let errors=0;
 
 if [[ ! -x $MINCLOOKUP_BIN ]]; then
-    MINCLOOKUP_BIN=`which minclookup`;
+    #MINCLOOKUP_BIN=`which minclookup`;
+    # probably not testing the right binary
+    exit 1
 fi
 
 if [[ ! -x $MINCEXTRACT_BIN ]]; then
@@ -11,11 +13,12 @@ if [[ ! -x $MINCEXTRACT_BIN ]]; then
 fi
 
 echo -n test label remap
+
 # Test a simple map case
 $MINCLOOKUP_BIN -clobber -discrete -quiet -lut_string "1 2; 2 3" test-labels.mnc minclookup-labels-out.mnc
 
 l_out_2=`$MINCEXTRACT_BIN -nonormalize minclookup-labels-out.mnc | grep -c 2`
-l_in_1=`$MINCEXTRACT -nonormalize test-labels.mnc | grep -c 1`
+l_in_1=`$MINCEXTRACT_BIN -nonormalize test-labels.mnc | grep -c 1`
 
 if [[ $l_out_2 != "$l_in_1" ]]; then
     echo "Problem with label per-voxel remapping:" $l_in_1 $l_out_1
