b49a2b44564d9b0f43fcb64da1a97fb58eee5a75
Steve M. Robbins <smr@debian.org>
2020-07-25 11:36:24 -0500

    Add "extern" modifier to matrix_errno and matrix_errtxt declarations in ecat_write.h; define these two in ecat_write.c. Fixes build error using GCC-10 that now defaults to -fno-common; see https://gcc.gnu.org/gcc-10/porting_to.html

--- conversion/minctoecat/ecat_write.h
+++ conversion/minctoecat/ecat_write.h
@@ -39,9 +39,9 @@ typedef enum {
   BitData
 } MatrixDataType;
 
-MatrixErrorCode matrix_errno;
+extern MatrixErrorCode matrix_errno;
 
-char matrix_errtxt[132];
+extern char matrix_errtxt[132];
 
 typedef struct XMAIN_HEAD {
   char magic_number[14];
